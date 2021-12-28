b0aa0ab3c92f9653c4f5b34a520a4d6637a82733
Michal Hicz <michal07.h@gmail.com>
2021-12-28 23:57:04 +0100

    ip

--- router.py
+++ router.py
@@ -6,11 +6,11 @@ import socket
 tun = TunTap(nic_type="Tun", nic_name="hnet-testnet")
 tun.config(ip="192.168.99.1", mask="255.255.255.0")
 
-HOST = '127.0.0.1'
-PORT = 65432
+HOST = '10.0.157.101' # (socket.gethostbyname(socket.gethostname())
+PORT = 654321
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((socket.gethostbyname(socket.gethostname()), PORT))
+    s.bind(HOST), PORT))
     s.listen()
 
     while True:
