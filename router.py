932b8a9f59a287c911360371b3c3ee94049d073c
Michal Hicz <michal07.h@gmail.com>
2021-12-29 00:18:12 +0100

    s

--- router.py
+++ router.py
@@ -9,16 +9,16 @@ tun.config(ip="192.168.99.1", mask="255.255.255.0")
 HOST = '10.0.157.101' # (socket.gethostbyname(socket.gethostname())
 PORT = 65432
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    while True:
-        try:
-            conn, addr = s.accept()
-            data = conn.recv(1024)
-            tun.write(data)
-            if not data:
-                break
-        except KeyboardInterrupt:
-            s.close()
-            tun.close()
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen()
+while True:
+    try:
+        conn, addr = s.accept()
+        data = conn.recv(1024)
+        tun.write(data)
+        if not data:
+           break
+    except KeyboardInterrupt:
+        s.close()
+        tun.close()
