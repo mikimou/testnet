6f93c83d255308c24193666b1d1a6b3fb05077c6
Michal Hicz <michal07.h@gmail.com>
2021-12-28 23:20:56 +0100

    sockets

--- router.py
+++ router.py
@@ -1,9 +1,23 @@
+#!/usr/bin/env python3
+
 from tuntap import TunTap
+import socket
 
 tun = TunTap(nic_type="Tun", nic_name="hnet-testnet")
 tun.config(ip="192.168.99.1", mask="255.255.255.0")
 
-data = "steam"
-tun.write(data)
+HOST = '127.0.0.1'
+PORT = 65432
 
-input()
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print('Connected by', addr)
+        while True:
+            data = conn.recv(1024)
+            tun.write(data)
+            if not data:
+                break
+            conn.sendall(data)
