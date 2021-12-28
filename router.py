2aace1b0122923d0e1050939fe54dff7ea69ac4b
Michal Hicz <michal07.h@gmail.com>
2021-12-28 23:51:01 +0100

    localhost

--- router.py
+++ router.py
@@ -10,14 +10,12 @@ HOST = '127.0.0.1'
 PORT = 65432
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
+    s.bind((socket.gethostbyname(socket.gethostname), PORT))
     s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print('Connected by', addr)
-        while True:
-            data = conn.recv(1024)
-            tun.write(data)
-            if not data:
-                break
-            conn.sendall(data)
+
+    while True:
+        conn, addr = s.accept()
+        data = conn.recv(1024)
+        tun.write(data)
+        if not data:
+            break
