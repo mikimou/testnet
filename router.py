afaaa65694bd0349973ee9ce6d373c4198c8173c
Michal Hicz <michal07.h@gmail.com>
2021-12-29 01:17:14 +0100

    s

--- router.py
+++ router.py
@@ -12,18 +12,32 @@ PORT = 65432
 tun = TunTap(nic_type="Tun", nic_name="hnet-testnet")
 tun.config(ip="192.168.99.1", mask="255.255.255.0")
 
-try:
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((HOST, PORT))
-        #s.listen()
-        while True:
-            conn, addr = s.accept()
-            with conn:
-                    while True:
-                        data = conn.recv(1024)
-                        tun.write(data)
-                        if not data:
-                            break
-                        conn.sendall(data)
-except KeyboardInterrupt:
-    tun.close()
+def tcp_handler():
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind((HOST, PORT))
+            s.listen()
+            while True:
+                conn, addr = s.accept()
+                with conn:
+                        while True:
+                            data = conn.recv(1024)
+                            tun.write(data)
+                            if not data:
+                                break
+                            conn.sendall(data)
+    except KeyboardInterrupt:
+        tun.close()
+
+def udp_handler():
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+            s.bind((HOST, PORT))
+            while True:
+                data, addr = s.recv(1024)
+                tun.write(data)
+                if not data:
+                    break
+                s.sendto(data, "178.41.16.171", PORT)
+    except KeyboardInterrupt:
+        tun.close()
